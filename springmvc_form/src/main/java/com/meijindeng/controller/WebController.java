7e20dbe1ac242c729a16310744cef889d0f2ed1e
meijindeng <meijindeng@126.com>
2021-09-13 15:28:24 +0800

    2021-9-13 静态页面

---
+++ springmvc_form/src/main/java/com/meijindeng/controller/WebController.java
@@ -0,0 +1,24 @@
+package com.meijindeng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @description: WebController
+ * @Author: dengmeijin
+ * @Date: 2021/9/13 14:53
+ */
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/staticPage", method = RequestMethod.GET)
+    public String redirect() {
+        return "redirect:/pages/final.html";//重定向
+    }
+}
